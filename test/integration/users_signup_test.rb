6bdf42c46601fdb32d77781a7a57878230c404db
Andy Kwong <andyk314@gmail.com>
2015-05-30 14:32:03 -0700

    Update tests for users signup page.

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -1,30 +1,28 @@
 require 'test_helper'
 
 class UsersSignupTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
-
-  test "invalid signup information" do
+  test 'invalid signup information' do
     get signup_path
     assert_no_difference 'User.count' do
-      post users_path, user:  { name: "",
-                                email: "user@invalid",
-                                password: "foo",
-                                password_confirmation: "bar" }
+      post users_path, user:  { name: '',
+                                email: 'user@invalid',
+                                password: 'foo',
+                                password_confirmation: 'bar' }
     end
     assert_template 'users/new'
+    assert_select 'div#error_explanation'
+    assert_select 'div.field_with_errors'
   end
 
-  test "valid signup information" do
+  test 'valid signup information' do
     get signup_path
     assert_difference 'User.count' do
-      post_via_redirect users_path, user: { name: "Example User",
-                                            email: "user@example.com",
-                                            password: "password",
-                                            password_confirmation: "password" }
+      post_via_redirect users_path, user: { name: 'Example User',
+                                            email: 'user@example.com',
+                                            password: 'password',
+                                            password_confirmation: 'password' }
     end
     assert_template 'users/show'
+    assert_not flash.empty?
   end
-
 end
